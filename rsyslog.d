49e0cb3ea6c698e6966a13ab4fba40189388dabc
ashokchokalingam <33452485+ashokchokalingam@users.noreply.github.com>
2024-12-23 07:45:59 +0400

    Create rsyslog.d

---
+++ rsyslog.d
@@ -0,0 +1,27 @@
+###########################
+#### MODULES ####
+###########################
+$MaxMessageSize 64k
+
+module(load="imfile")
+
+template(name="SingleLineJSONFormat" type="string" string="%msg%\n")
+
+###########################
+#### INPUTS ####
+###########################
+
+input(type="imfile"
+      File="/var/log/logstash/anomalyhunter/*.json"
+      Tag="anomalyhunter"
+      Severity="info"
+      Facility="local6"
+      readMode="0"
+      escapeLF="off"
+)
+
+###########################
+#### OUTPUTS ####
+###########################
+
+if $syslogtag == 'anomalyhunter' then @@172.16.0.52:514;SingleLineJSONFormat
